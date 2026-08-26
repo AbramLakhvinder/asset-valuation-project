55ea8c3cd5627ae224a091df5b55fb3beec9cb1e
Abram Lakhvinder <a.lakhvinder02@gmail.com>
2026-08-26 17:20:55 +0200

    refactor(preprocessing): généraliser le nettoyage des valeurs hors bornes
    traitement_valeurs_incohérentes (seuils YearMade en dur) remplacée par nettoyer_hors_bornes(dataframe, colonne, borne_min, borne_max), réutilisable sur n'importe quelle colonne. Ajout de variable_nan_structurel (typologie des NaN par groupe) et imputer_valeurs_rares (imputation ciblée par seuil).

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,8 +10,11 @@ def regrouper_categories_rares(train, test, colonne, seuil):
     test2.loc[~(test2[colonne].isin(categories_gardees)), colonne] = "Other"
     return train2, test2
 
-def traitement_valeurs_incohérentes(dataframe, colonne):
-    dataframe.loc[((dataframe[colonne]==1000)|(dataframe[colonne]==1919)|(dataframe[colonne]>2012)), colonne] = np.nan
+import numpy as np
+
+def nettoyer_hors_bornes(dataframe, colonne, borne_min, borne_max):
+    mask_invalide = (dataframe[colonne] < borne_min) | (dataframe[colonne] > borne_max)
+    dataframe.loc[mask_invalide, colonne] = np.nan
     return dataframe
 
 def zero_vers_nan(dataframe, colonne): 
@@ -52,3 +55,48 @@ def imputation_mediane_a_nan(train, test, colonnegroupement, colonneimputer, seu
     test2[colonneimputer] = test2[colonneimputer].fillna(valeur_a_imputer_test)
     return train2, test2
 
+def variable_nan_structurel(base, groupbycolonne, listecolonne, seuilbas=10, seuilhaut=80): 
+    variablegerdees = []
+    
+    for i in listecolonne: 
+        a = 0  
+        b = 0  
+        # On va compter le volume total de données dans la zone grise
+        volume_ventre_mou = 0
+        total_observations = 0
+        
+        df_nan_pct = base.groupby(groupbycolonne)[i].apply(lambda x: x.isnull().mean() * 100)
+        df_total_size = base.groupby(groupbycolonne)[i].size()
+        
+        for modalite, pct_nan in df_nan_pct.items():
+            taille_groupe = df_total_size.loc[modalite]
+            total_observations += taille_groupe
+            
+            # Si on est dans la zone grise (indépendamment de la taille du groupe)
+            if seuilbas < pct_nan < seuilhaut:
+                volume_ventre_mou += taille_groupe
+                
+            # On vérifie les extrêmes uniquement pour les groupes représentatifs (>= 385)
+            elif taille_groupe >= 385:
+                if pct_nan <= seuilbas:
+                    a += 1
+                elif pct_nan >= seuilhaut:
+                    b += 1
+                    
+        # RÈGLE MÉTIER : On rejette si plus de 5% du jeu de données total est dans la zone grise
+        tolerance_ventre_mou = (volume_ventre_mou / total_observations) < 0.05
+                        
+        if (a >= 1) and (b >= 1) and tolerance_ventre_mou:
+            variablegerdees.append(i)
+
+    return variablegerdees
+
+def imputer_valeurs_rares(dataframe, col_groupe, col_calcul, seuil_pourcentage, valeur_remplacement): 
+
+    for i in col_calcul:
+         taux_nan = dataframe.groupby(col_groupe)[i].apply(lambda x: x.isnull().mean() * 100)
+         groupes_a_changer = taux_nan[taux_nan > seuil_pourcentage].index 
+         mask = dataframe[col_groupe].isin(groupes_a_changer) & dataframe[i].isna()
+         dataframe.loc[mask, i] = valeur_remplacement
+    return dataframe
+        
